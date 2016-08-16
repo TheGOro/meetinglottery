ed665e61d483fe9bb7aa72c4b56bc023793f2971
Gabor Orosz <goro@goro.io>
2016-08-16 22:43:16 +0200

    Add Candidate and Participation models
    Signed-off-by: Gabor Orosz <goro@goro.io>

--- main/models.py
+++ main/models.py
@@ -2,4 +2,31 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=30,
+                            unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Participation(models.Model):
+    ALONE    = 'A'
+    TOGETHER = 'T'
+    VACATION = 'V'
+    CLASSIFICATION_CHOICES = (
+        (ALONE, 'Alone'),
+        (TOGETHER, 'Together'),
+        (VACATION, 'Vacation')
+    )
+    candidate = models.ForeignKey(Candidate,
+                                  related_name="candidate")
+    classification = models.CharField(max_length=2,
+                                     choices=CLASSIFICATION_CHOICES,
+                                     default=ALONE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (self.date.strftime('%Y. %m. %d. %H:%M') + ' - ' + self.candidate.name +
+                ' (' + self.classification + ')')
